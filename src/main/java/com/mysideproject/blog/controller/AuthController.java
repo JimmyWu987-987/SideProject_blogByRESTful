5b569ea5517331d335d04c2def29c26eb288474c
JimmyWu <workjimmywu0820@gmail.com>
2025-12-15 08:42:24 +0800

    刪除多餘的程式

--- src/main/java/com/mysideproject/blog/controller/AuthController.java
+++ src/main/java/com/mysideproject/blog/controller/AuthController.java
@@ -84,12 +84,6 @@ public class AuthController {
         				)
         );
         
-        try {
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-        
         // 2. 驗證成功後，將 Authentication 物件放入 SecurityContext 中
         SecurityContextHolder.getContext().setAuthentication(authentication);
         
