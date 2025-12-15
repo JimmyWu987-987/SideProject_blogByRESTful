ec8fc857267654f6c2d1993f53628c4d94f3507b
JimmyWu <workjimmywu0820@gmail.com>
2025-12-15 11:48:21 +0800

    新增控制前端畫面的api

---
+++ src/main/java/com/mysideproject/blog/controller/ViewController.java
@@ -0,0 +1,14 @@
+package com.mysideproject.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+	
+	@GetMapping("/blog/posts")
+	public String showPostListPage() {
+		return "forward:/front_end/blog/post/index.html";
+	}
+
+}
